346a7e109ca7fe53152275854da00267f0aa058d
nu_kotov <>
2025-10-03 01:38:13 +0300

    Ручка получения бинарных данных из miniiio.

--- internal/handler/binary.go
+++ internal/handler/binary.go
@@ -17,7 +17,7 @@ import (
 
 type BinaryStorage interface {
 	InsertBinaryData(context.Context, string, string, io.ReadCloser) (*minio.UploadInfo, error)
-	// SelectBinaryData(context.Context, string, string) (string, error)
+	SelectBinaryData(context.Context, string, string) (*minio.Object, error)
 	// DeleteBinaryData(context.Context, string, string) error
 }
 
@@ -38,7 +38,7 @@ func NewBinaryHandler(router *chi.Mux, cfg *config.Config, storage BinaryStorage
 	)
 
 	router.Post(`/api/binary/add`, middlewareStack(handler.AddBinary()))
-	// router.Post(`/api/binary/get`, middlewareStack(handler.GetBinary()))
+	router.Post(`/api/binary/get`, middlewareStack(handler.GetBinary()))
 	// router.Post(`/api/binary/delete`, middlewareStack(handler.DeleteBinary()))
 
 }
@@ -86,48 +86,58 @@ func (handler *BinaryHandler) AddBinary() http.HandlerFunc {
 	}
 }
 
-// func (handler *BinaryHandler) GetBinary() http.HandlerFunc {
-// 	return func(res http.ResponseWriter, req *http.Request) {
-// 		token, err := req.Cookie("token")
+func (handler *BinaryHandler) GetBinary() http.HandlerFunc {
+	return func(res http.ResponseWriter, req *http.Request) {
+		token, err := req.Cookie("token")
 
-// 		if err != nil {
-// 			logger.Log.Info(err.Error())
-// 			res.WriteHeader(http.StatusUnauthorized)
-// 			return
-// 		}
+		if err != nil {
+			logger.Log.Info(err.Error())
+			res.WriteHeader(http.StatusUnauthorized)
+			return
+		}
 
-// 		userID, err := auth.GetUserID(token.Value, handler.Config.SecretKey)
-// 		if err != nil {
-// 			logger.Log.Info(err.Error())
-// 			http.Error(res, err.Error(), http.StatusBadRequest)
-// 			return
-// 		}
+		userID, err := auth.GetUserID(token.Value, handler.Config.SecretKey)
+		if err != nil {
+			logger.Log.Info(err.Error())
+			http.Error(res, err.Error(), http.StatusBadRequest)
+			return
+		}
 
-// 		body, err := io.ReadAll(req.Body)
-// 		if err != nil {
-// 			logger.Log.Info(err.Error())
-// 			http.Error(res, "Invalid body", http.StatusBadRequest)
-// 			return
-// 		}
+		body, err := io.ReadAll(req.Body)
+		if err != nil {
+			logger.Log.Info(err.Error())
+			http.Error(res, "Invalid body", http.StatusBadRequest)
+			return
+		}
 
-// 		var jsonBody models.TextID
-// 		if err = json.Unmarshal(body, &jsonBody); err != nil {
-// 			http.Error(res, err.Error(), http.StatusBadRequest)
-// 			return
-// 		}
+		filename := string(body)
+		miniioFilename := fmt.Sprintf("%s/%s", userID, filename)
 
-// 		txt, err := handler.Storage.SelectBinaryData(req.Context(), userID, jsonBody.DataID)
-// 		if err != nil {
-// 			logger.Log.Info(err.Error())
-// 			http.Error(res, "Select text data error", http.StatusInternalServerError)
-// 			return
-// 		}
+		file, err := handler.Storage.SelectBinaryData(req.Context(), handler.Config.MiniIOConnection.BucketName, miniioFilename)
+		if err != nil {
+			logger.Log.Info(err.Error())
+			http.Error(res, "Select binary data error", http.StatusInternalServerError)
+			return
+		}
+		defer file.Close()
 
-// 		res.Header().Set("Content-Type", "text/plain")
-// 		res.WriteHeader(http.StatusOK)
-// 		io.WriteString(res, txt)
-// 	}
-// }
+		info, err := file.Stat()
+		if err != nil {
+			logger.Log.Info(err.Error())
+			http.Error(res, "Error stating object", http.StatusInternalServerError)
+			return
+		}
+
+		res.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=\"%s\"", filename))
+		res.Header().Set("Content-Type", "application/octet-stream")
+		res.Header().Set("Content-Length", fmt.Sprintf("%d", info.Size))
+
+		if _, err = io.Copy(res, file); err != nil {
+			logger.Log.Info(err.Error())
+			http.Error(res, "Failed to stream file", http.StatusInternalServerError)
+		}
+	}
+}
 
 // func (handler *BinaryHandler) DeleteBinary() http.HandlerFunc {
 // 	return func(res http.ResponseWriter, req *http.Request) {
--- internal/storage/locminiio/binary.go
+++ internal/storage/locminiio/binary.go
@@ -22,16 +22,16 @@ func (bin *BinaryStorage) InsertBinaryData(ctx context.Context, bucketName strin
 	return &info, nil
 }
 
-// func (bin *BinaryStorage) SelectBinaryData(ctx context.Context, bucketName string, filename string, body io.ReadCloser) (*minio.UploadInfo, error) {
-// 	info, err := bin.Stor.PutObject(ctx, bucketName, filename, body, -1, minio.PutObjectOptions{
-// 		ContentType: "application/octet-stream",
-// 	})
-// 	if err != nil {
-// 		return nil, err
-// 	}
+func (bin *BinaryStorage) SelectBinaryData(ctx context.Context, bucketName string, filename string) (*minio.Object, error) {
 
-// 	return &info, nil
-// }
+	file, err := bin.Stor.GetObject(context.Background(), bucketName, filename, minio.GetObjectOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	return file, nil
+}
+
 // func (bin *BinaryStorage) DeleteBinaryData(ctx context.Context, bucketName string, filename string, body io.ReadCloser) (*minio.UploadInfo, error) {
 // 	info, err := bin.Stor.PutObject(ctx, bucketName, filename, body, -1, minio.PutObjectOptions{
 // 		ContentType: "application/octet-stream",
